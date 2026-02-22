a43e5fe2c10ce83dd6fa21a2e52aba133098f5ee
Notion-Roleplay <timberland16@hotmail.de>
2026-02-22 15:25:37 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+-----------------Für Support, Skripte und mehr----------------
+---------------- https://discord.gg/H2tpQtvBf7 ---------------
+--------------------------------------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Notion-Roleplay © 2026'
+description 'Notion-Roleplay - Police Vehicle Laser'
+version '1.5.0'
+
+ui_page 'html/index.html'
+
+shared_script 'config.lua'
+
+client_script {
+    'client.lua'
+}
+
+server_script {
+    'server/server.lua',
+    'server/updater.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
